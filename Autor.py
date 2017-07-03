ece44431fa580551777251b44e4cbadf2e39ab12
Mateus Bittencourt <mateus.lbittencourt@gmail.com>
2017-07-03 14:07:17 -0300

    Add a classe Autor

---
+++ Autor.py
@@ -0,0 +1,14 @@
+class Autor(object):
+
+    def __init__(self, cod, nome):
+        self._cod = cod
+        self._nome = nome
+
+
+    def getCod(self):
+        return self._cod
+
+    def getNome(self):
+        return self._nome
+
+
