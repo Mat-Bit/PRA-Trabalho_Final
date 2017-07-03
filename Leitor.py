8be050afbef765164a28e3975309e5c5eb840028
Mateus Bittencourt <mateus.lbittencourt@gmail.com>
2017-07-03 14:08:12 -0300

    Add a classe Leitor

---
+++ Leitor.py
@@ -0,0 +1,13 @@
+class Leitor(object):
+    
+    def __init__(self, cod, nome):
+        self._cod = cod
+        self._nome = nome
+
+
+    def getCod(self):
+        return self._cod
+
+    def getNome(self):
+        return self._nome
+
