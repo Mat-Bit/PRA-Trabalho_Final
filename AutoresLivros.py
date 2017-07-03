d4a86b63e0f3d96c7ac5e2c35ec7b126b59d33a8
Mateus Bittencourt <mateus.lbittencourt@gmail.com>
2017-07-03 14:06:19 -0300

    Add a classe AutoresLivros

---
+++ AutoresLivros.py
@@ -0,0 +1,13 @@
+class AutoresLivros(object):
+
+    def __init__(self, codL, codA):
+        self._codL = codL
+        self._codA = codA
+
+
+    def getCodL(self):
+        return self._codL
+
+    def getCodA(self):
+        return self._codA
+
