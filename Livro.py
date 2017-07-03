399c239bbc97ea29e0060e1adfc5080d02cadb97
Mateus Bittencourt <mateus.lbittencourt@gmail.com>
2017-07-03 14:09:11 -0300

    Add a clase Livro

---
+++ Livro.py
@@ -0,0 +1,16 @@
+class Livro(object):
+
+    def __init__(self, cod, nome):
+        self._cod = cod
+        self._nome = nome
+
+
+    def getCod(self):
+        return self._cod
+
+    def getNome(self):
+        return self._nome
+
+
+
+
